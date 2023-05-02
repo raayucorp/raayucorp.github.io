0ffaf40d083d3a09c96ce39ccc66be9c89661ba9
Raayu <98719494+raayucorp@users.noreply.github.com>
2023-05-02 14:54:45 +0900

    Create move.js

---
+++ move.js
@@ -0,0 +1,5 @@
+$("#particle").particleText({
+text: "Raayu<br>@raayucorp.<br>らーゆのウェブサイト", // 表示させたいテキスト。改行の場合は<br>追加
+colors:["#000000","#000000", "#000000"], // パーティクルの色を複数指定可能
+speed: "high", // slow, middle, high の3つから粒子が集まる速さを選択
+});
